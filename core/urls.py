4eb7e0081055788ea4faf961f4f65a387cbec574
Fernando Pereira <fernando.pereira_contractor@celanese.com>
2025-06-16 15:16:00 -0300

    adicionando dashboard

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProdutorViewSet, PropriedadeViewSet, SafraViewSet, CulturaViewSet
+from .views import DashboardAPIView
 
 router = DefaultRouter()
 router.register(r'produtores', ProdutorViewSet)
@@ -9,4 +10,5 @@ router.register(r'safras', SafraViewSet)
 router.register(r'culturas', CulturaViewSet)
 urlpatterns = [
     path('', include(router.urls)),
+    path('dashboard/', DashboardAPIView.as_view(), name='dashboard'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Produtor, Propriedade, Safra, Cultura
 from .serializers import ProdutorSerializer, PropriedadeSerializer, SafraSerializer, CulturaSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Sum, Count
+
 
 class ProdutorViewSet(viewsets.ModelViewSet):
     queryset = Produtor.objects.all()
@@ -18,3 +22,40 @@ class SafraViewSet(viewsets.ModelViewSet):
 class CulturaViewSet(viewsets.ModelViewSet):
     queryset = Cultura.objects.all()
     serializer_class = CulturaSerializer
+
+class DashboardAPIView(APIView):
+    def get(self, request):
+        total_fazendas = Propriedade.objects.count()
+        total_hectares = Propriedade.objects.aggregate(
+            total_area=Sum('area_total_hectares')
+        )['total_area'] or 0
+
+        propriedades_por_estado = (
+            Propriedade.objects
+            .values('estado')
+            .annotate(total=Count('id'))
+            .order_by('-total')
+        )
+
+        culturas_por_tipo = (
+            Cultura.objects
+            .values('cultura_plantada')
+            .annotate(total=Count('id'))
+            .order_by('-total')
+        )
+
+        uso_solo = Propriedade.objects.aggregate(
+            total_agricultavel=Sum('area_agriculturavel_hectares'),
+            total_vegetacao=Sum('area_vegetacao_hectares')
+        )
+
+        return Response({
+            "total_fazendas": total_fazendas,
+            "total_hectares": total_hectares,
+            "propriedades_por_estado": list(propriedades_por_estado),
+            "culturas_por_tipo": list(culturas_por_tipo),
+            "uso_do_solo": {
+                "agricultavel": uso_solo['total_agricultavel'] or 0,
+                "vegetacao": uso_solo['total_vegetacao'] or 0
+            }
+        })
